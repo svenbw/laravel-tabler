76ee67a4a183b10f43e80660d8cb3e30c5f3b5bd
Sven Rymenants <sven@byteworks.be>
2026-09-17 19:42:12 +0200

    Fix icon position

--- resources/views/components/partials/header/notifications.blade.php
+++ resources/views/components/partials/header/notifications.blade.php
@@ -1,7 +1,7 @@
 @use('Svenbw\LaravelTabler\Tabler')
 @php $in_sidebar = isset($in_sidebar) ? $in_sidebar : false; @endphp
 <div @class(['nav-item dropdown d-none d-md-flex', 'me-3' => !$in_sidebar])>
-    <a href="#" class="nav-link px-0" data-bs-toggle="dropdown" tabindex="-1" aria-label="{{ __('tabler::notifications.show_notifications') }}">
+    <a href="#" class="nav-link px-0 justify-content-center" data-bs-toggle="dropdown" tabindex="-1" aria-label="{{ __('tabler::notifications.show_notifications') }}">
         <svg xmlns="http://www.w3.org/2000/svg" class="icon" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
             <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
             <path d="M10 5a2 2 0 0 1 4 0a7 7 0 0 1 4 6v3a4 4 0 0 0 2 3h-16a4 4 0 0 0 2 -3v-3a7 7 0 0 1 4 -6"/>
--- resources/views/components/partials/header/theme-switch.blade.php
+++ resources/views/components/partials/header/theme-switch.blade.php
@@ -1,6 +1,6 @@
-<button type="button"  class="nav-link px-0 hide-theme-dark" title="Enable dark mode" data-bs-toggle="tooltip" data-bs-placement="bottom" data-theme-switch="dark">
+<button type="button"  class="nav-link px-0 justify-content-center hide-theme-dark" title="Enable dark mode" data-bs-toggle="tooltip" data-bs-placement="bottom" data-theme-switch="dark">
     <svg xmlns="http://www.w3.org/2000/svg" class="icon" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M12 3c.132 0 .263 0 .393 0a7.5 7.5 0 0 0 7.92 12.446a9 9 0 1 1 -8.313 -12.454z" /></svg>
 </button>
-<button type="button" class="nav-link px-0 hide-theme-light" title="Enable light mode" data-bs-toggle="tooltip" data-bs-placement="bottom" data-theme-switch="light">
+<button type="button" class="nav-link px-0 justify-content-center hide-theme-light" title="Enable light mode" data-bs-toggle="tooltip" data-bs-placement="bottom" data-theme-switch="light">
     <svg xmlns="http://www.w3.org/2000/svg" class="icon" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M12 12m-4 0a4 4 0 1 0 8 0a4 4 0 1 0 -8 0" /><path d="M3 12h1m8 -9v1m8 8h1m-9 8v1m-6.4 -15.4l.7 .7m12.1 -.7l-.7 .7m0 11.4l.7 .7m-12.1 -.7l-.7 .7" /></svg>
 </button>
