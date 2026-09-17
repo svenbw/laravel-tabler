c2d435677913759f18dd182fadb51d6c33d847fc
Sven Rymenants <sven@byteworks.be>
2026-09-17 19:19:28 +0200

    Update footer add burger menu to profile

--- resources/views/components/partials/footer/bottom.blade.php
+++ resources/views/components/partials/footer/bottom.blade.php
@@ -1,11 +1,6 @@
 <footer class="footer footer-transparent d-print-none">
     <div class="container-xl">
         <div class="row text-center align-items-center flex-row-reverse">
-            <div class="col-lg-auto ms-lg-auto">
-                <ul class="list-inline list-inline-dots mb-0">
-                    <li class="list-inline-item"><a href="https://github.com/svenbw/csa" target="_blank" class="link-secondary" rel="noopener">Source code</a></li>
-                </ul>
-            </div>
             <div class="col-12 col-lg-auto mt-3 mt-lg-0">
                 <ul class="list-inline list-inline-dots mb-0">
                     <li class="list-inline-item">
--- resources/views/components/partials/header/profile-right.blade.php
+++ resources/views/components/partials/header/profile-right.blade.php
@@ -4,12 +4,13 @@
     $user = Auth()->user();
 @endphp
     <div class="nav-item dropdown">
-        <a href="#" class="nav-link d-flex lh-1 text-reset p-0" data-bs-toggle="dropdown" aria-label="{{ __('tabler::menu.user') }}">
+        <a href="#" class="nav-link d-flex align-items-center lh-1 text-reset p-0" data-bs-toggle="dropdown" aria-label="{{ __('tabler::menu.user') }}">
             <span class="avatar">{{ $user->initials }}</span>
             <div class="d-none d-xl-block ps-2">
                 <div>{{ $user->name }}</div>
-                <div class="mt-1 small text-muted">Software Engineer</div>
+                <div class="mt-1 small text-muted">{{ $user->first_name }}</div>
             </div>
+            <x-tabler-menu-2 class="icon ms-auto d-none d-lg-block" />
         </a>
         <div class="dropdown-menu dropdown-menu-end dropdown-menu-arrow">
             @foreach(app(Tabler::class)->menu()->getGroup('header')->items() as $item)
